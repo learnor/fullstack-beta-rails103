54489570b30cb99d822e667a2801d537ef4ef96c
Learnor <learnor@gmail.com>
2016-12-01 13:47:46 -0600

    implement group#edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,6 +17,16 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
   end
 
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    @group.update(group_params)
+    
+  end
+
   private
   def group_params
     params.require(:group).permit(:title,:description)
