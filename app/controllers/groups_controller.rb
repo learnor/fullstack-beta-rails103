73fac46d5950b1daf6f9f16cff8fc58ef3da0c6c
Learnor <learnor@gmail.com>
2016-12-02 15:50:59 -0600

    when created a group, join it automatically

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -13,6 +13,7 @@ class GroupsController < ApplicationController
     @group = Group.new(group_params)
     @group.user = current_user
     if @group.save
+      current_user.join(@group)
       redirect_to groups_path
     else
       render :new
@@ -38,6 +39,32 @@ class GroupsController < ApplicationController
   def destroy
     @group.destroy
     redirect_to groups_path, alert: "Group Deleted"
+  end
+
+  def join
+    @group = Group.find(params[:id])
+
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = 'Join Group Success!'
+    else
+      flash[:warning] = 'Already a Member!'
+    end
+
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = 'Quit Group Success!'
+    else
+      flash[:warning] = 'Not a Member Yet!'
+    end
+
+    redirect_to group_path(@group)
   end
 
   private
